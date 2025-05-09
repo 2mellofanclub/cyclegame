61d4f87cb6f4b5da09b54d4251ca8f681a3d7841
2mellofanclub <rcarlsson0110@gmail.com>
2025-05-10 01:53:58 +0300

    40: base main menu done

--- main.gd
+++ main.gd
@@ -1,18 +1,14 @@
 extends Node
 
+var in_menu := true
+
 
 func _ready():
-	
 	$LevelController.start_main_menu()
-	#$LevelController.start_new_level("res://levels/grid.tscn")
-	#await get_tree().create_timer(30).timeout
-	#$LevelController.start_new_level("res://levels/one_v_one.tscn")
-	#$LevelController.start_new_level("res://levels/the_range.tscn")
-	#$LevelController.start_new_level("res://levels/burning_transistor.tscn")
-	
+
 
 func _process(delta):
-	if Input.is_action_just_pressed("ui_cancel"):
+	if Input.is_action_just_pressed("ui_cancel") and not in_menu:
 		if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
 			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 		else:
--- main_menu.gd
+++ main_menu.gd
@@ -1,3 +1,55 @@
 extends Control
 
-var level_controller: Node3D
+enum MENU_STATE {MAIN, MODES, SETTINGS, CREDITS, EXIT, SAVES, GARAGE, CW_LEVELS, DH_LEVELS, SURV_LEVELS}
+
+var level_controller : Node3D
+var current_menu_state : MENU_STATE
+
+
+func go_back():
+	match current_menu_state:
+		MENU_STATE.MODES:
+			change_menu_state(MENU_STATE.MAIN)
+		MENU_STATE.SETTINGS:
+			change_menu_state(MENU_STATE.MAIN)
+		MENU_STATE.CREDITS:
+			change_menu_state(MENU_STATE.MAIN)
+		MENU_STATE.GARAGE:
+			change_menu_state(MENU_STATE.MODES)
+		MENU_STATE.CW_LEVELS:
+			change_menu_state(MENU_STATE.MODES)
+		MENU_STATE.DH_LEVELS:
+			change_menu_state(MENU_STATE.MODES)
+		MENU_STATE.SURV_LEVELS:
+			change_menu_state(MENU_STATE.MODES)
+
+
+func change_menu_state(state : MENU_STATE):
+	current_menu_state = state
+	$Back.show()
+	for child in $MainCenterContainer.get_children():
+		child.hide()
+	match state:
+		MENU_STATE.MAIN:
+			$Back.hide()
+			$MainCenterContainer/MainControls.show()
+		MENU_STATE.MODES:
+			$MainCenterContainer/Modes.show()
+		MENU_STATE.SETTINGS:
+			pass
+		MENU_STATE.CREDITS:
+			pass
+		MENU_STATE.EXIT:
+			get_tree().quit()
+		MENU_STATE.GARAGE:
+			pass
+		MENU_STATE.CW_LEVELS:
+			$MainCenterContainer/CycleWarLevels.show()
+		MENU_STATE.DH_LEVELS:
+			$MainCenterContainer/DataHuntLevels.show()
+		MENU_STATE.SURV_LEVELS:
+			$MainCenterContainer/SurvivalLevels.show()
+
+
+func start_level(path : String):
+	level_controller.start_new_level(path)
